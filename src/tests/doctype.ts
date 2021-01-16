7e9425e646bbbf54b86275afb2a3122bd251174a
Raúl López <hi@raullg.com>
2021-01-16 23:26:54 +0000

    Create doctype.ts

---
+++ src/tests/doctype.ts
@@ -0,0 +1,24 @@
+import { Test } from "../classes/test";
+import { Result } from "../types/result";
+import { ResultType } from "../enums/result-type";
+import { Importance } from "../enums/importance";
+
+export class DoctypeTest extends Test {
+    constructor () {
+        super('<!DOCTYPE html>', 'Server response starts with the HTML Doctype definition', 'https://www.w3schools.com/tags/tag_doctype.asp')
+    }
+
+    getType (): ResultType {
+        return this.isValid ? ResultType.Success : ResultType.Error
+    }
+
+    getImportance (): Importance {
+        return Importance.Low
+    }
+
+    async test (): Promise<Result> {
+        const html = this.browser.raw().toLowerCase().trim()
+        this.isValid = html.startsWith('<!doctype html>')
+        return this.getResult()
+    }
+}
