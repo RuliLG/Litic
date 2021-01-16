193d6bc5cacc6650e1d5bc3708d4defe5cb4aff4
Raúl López <hi@raullg.com>
2021-01-16 23:42:25 +0000

    Create plain-text-emails.ts

---
+++ src/tests/plain-text-emails.ts
@@ -0,0 +1,26 @@
+import { Test } from "../classes/test";
+import { Result } from "../types/result";
+import { ResultType } from "../enums/result-type";
+import { Importance } from "../enums/importance";
+import { EmailValidator } from "../validators/email-validator";
+
+export class PlainTextEmailsTest extends Test {
+    constructor () {
+        super('No plain text emails', 'Avoid displaying plain text emails to prevent email address harvesting.', 'https://seositecheckup.com/tools/plaintext-emails-test')
+    }
+
+    getType (): ResultType {
+        return this.isValid ? ResultType.Success : ResultType.Error
+    }
+
+    getImportance (): Importance {
+        return Importance.Low
+    }
+
+    async test (): Promise<Result> {
+        const html = await this.browser.html()
+        const hasEmails = (new EmailValidator()).isValid(html)
+        this.isValid = !hasEmails
+        return this.getResult()
+    }
+}
