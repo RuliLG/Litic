cd3afca472a50ff959d04ebf99d40b29f66d4a1f
Raúl López <hi@raullg.com>
2021-01-16 23:13:35 +0000

    Create https.ts

---
+++ src/tests/https.ts
@@ -0,0 +1,24 @@
+import { Test } from "../classes/test";
+import { Result } from "../types/result";
+import { ResultType } from "../enums/result-type";
+import { Importance } from "../enums/importance";
+
+export class HttpsTest extends Test {
+    constructor () {
+        super('HTTPS', 'Page is loaded through HTTPS')
+    }
+
+    getType (): ResultType {
+        return this.isValid ? ResultType.Success : ResultType.Error
+    }
+
+    getImportance (): Importance {
+        return Importance.High
+    }
+
+    async test (): Promise<Result> {
+        const html = await this.browser.html()
+        console.log(html)
+        return {} as Result
+    }
+}
