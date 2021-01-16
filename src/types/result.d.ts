4fbdba7b73f496fab8d5ec3e1feb12ec56e9dca0
Raúl López <hi@raullg.com>
2021-01-16 23:11:23 +0000

    Create result.d.ts

---
+++ src/types/result.d.ts
@@ -0,0 +1,12 @@
+import { Importance } from "../enums/importance";
+import { ResultType } from "../enums/result-type";
+
+export interface Result {
+    name: string,
+    description?: string,
+    type: ResultType,
+    infoUrl?: string,
+    passed?: boolean,
+    category?: string,
+    importance: Importance
+}
