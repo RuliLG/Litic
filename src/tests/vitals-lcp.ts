5d6e3dbe7203723c4c9f8a7d227dfd5ebfb9560d
Raúl López <hi@raullg.com>
2021-01-17 13:10:40 +0000

    Create vitals-lcp.ts

---
+++ src/tests/vitals-lcp.ts
@@ -0,0 +1,28 @@
+import { Test } from "../classes/test";
+import { Result } from "../types/result";
+import { ResultType } from "../enums/result-type";
+import { Importance } from "../enums/importance";
+import { LighthouseService } from "../services/lighthouse.service";
+
+export class VitalsLcpTest extends Test {
+    constructor () {
+        super('Largest Contentful Paint', 'To provide a good user experience, sites should strive to have Largest Contentful Paint occur within the first 2.5 seconds of the page starting to load.', 'https://web.dev/lcp/')
+    }
+
+    getType (): ResultType {
+        return this.isValid ? ResultType.Success : ResultType.Error
+    }
+
+    getImportance (): Importance {
+        return Importance.High
+    }
+
+    async test (): Promise<Result> {
+        const lighthouse = LighthouseService.get(this.browser.getUrl())
+        await lighthouse.run()
+        const report = lighthouse.getReport()['largest-contentful-paint']
+        this.isValid = report.numericValue <= 2500
+        this.comment = `LCP took ${report.displayValue}`
+        return this.getResult()
+    }
+}
