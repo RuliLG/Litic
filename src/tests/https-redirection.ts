3625075e3f4f8bb827e251835188f9249d8d48ed
Raúl López <hi@raullg.com>
2021-01-17 13:18:52 +0000

    Create https-redirection.ts

---
+++ src/tests/https-redirection.ts
@@ -0,0 +1,26 @@
+import { Test } from "../classes/test";
+import { Result } from "../types/result";
+import { ResultType } from "../enums/result-type";
+import { Importance } from "../enums/importance";
+import { LighthouseService } from "../services/lighthouse.service";
+
+export class HttpsRedirectionTest extends Test {
+    constructor () {
+        super('HTTPS Redirection', 'Make sure that you redirect all HTTP traffic to HTTPS in order to enable secure web features for all your users.', 'https://web.dev/redirects-http/')
+    }
+
+    getType (): ResultType {
+        return this.isValid ? ResultType.Success : ResultType.Error
+    }
+
+    getImportance (): Importance {
+        return Importance.High
+    }
+
+    async test (): Promise<Result> {
+        const lighthouse = LighthouseService.get(this.browser.getUrl())
+        const report = lighthouse.getReport()['redirects-http']
+        this.isValid = report.score === 1
+        return this.getResult()
+    }
+}
