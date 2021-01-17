3bd286693b7edb55f280007dd5d095b4a21e3df2
Raúl López <hi@raullg.com>
2021-01-17 11:04:43 +0000

    Create has-title.ts

---
+++ src/tests/has-title.ts
@@ -0,0 +1,37 @@
+import { Test } from "../classes/test";
+import { Result } from "../types/result";
+import { ResultType } from "../enums/result-type";
+import { Importance } from "../enums/importance";
+
+export class HasTitleTest extends Test {
+    constructor () {
+        super('<title> tag is present', 'HTML contains a non-empty <title> tag inside <head>', 'https://developers.google.com/search/docs/advanced/appearance/good-titles-snippets')
+    }
+
+    getType (): ResultType {
+        return this.isValid ? ResultType.Success : ResultType.Error
+    }
+
+    getImportance (): Importance {
+        return Importance.High
+    }
+
+    async test (): Promise<Result> {
+        const $ = await this.browser.getHtmlSoup()
+        const titleTags = $('head title')
+        if (titleTags.length === 1) {
+            const title = $(titleTags[0]).text().trim()
+            this.isValid = title.length > 0
+            this.comment = `Title: ${title}`
+        } else {
+            this.isValid = false
+            if (titleTags.length > 1) {
+                this.comment = `We found ${titleTags.length} different <title> tags and only one is allowed.`
+            } else {
+                this.comment = '<title> tag is mandatory.'
+            }
+        }
+
+        return this.getResult()
+    }
+}
