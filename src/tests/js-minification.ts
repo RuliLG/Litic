aebefa1e405cbbaad491abed2aded076ea7b452a
Raúl López <hi@raullg.com>
2021-01-17 13:44:44 +0000

    Create js-minification.ts

---
+++ src/tests/js-minification.ts
@@ -0,0 +1,26 @@
+import { Test } from "../classes/test";
+import { Result } from "../types/result";
+import { ResultType } from "../enums/result-type";
+import { Importance } from "../enums/importance";
+import { LighthouseService } from "../services/lighthouse.service";
+
+export class JsMinificationTest extends Test {
+    constructor () {
+        super('Minified JS', 'Minifying JavaScript files can reduce payload sizes and script parse time.', 'https://web.dev/unminified-javascript/')
+    }
+
+    getType (): ResultType {
+        return this.isValid ? ResultType.Success : ResultType.Error
+    }
+
+    getImportance (): Importance {
+        return Importance.High
+    }
+
+    async test (): Promise<Result> {
+        const lighthouse = LighthouseService.get(this.browser.getUrl())
+        const report = lighthouse.getReport()['unminified-javascript']
+        this.isValid = report.score === 1
+        return this.getResult()
+    }
+}
