b5104f16d577761178c9df87f82934503021707f
Raúl López <hi@raullg.com>
2021-01-17 13:42:54 +0000

    Create css-minification.ts

---
+++ src/tests/css-minification.ts
@@ -0,0 +1,26 @@
+import { Test } from "../classes/test";
+import { Result } from "../types/result";
+import { ResultType } from "../enums/result-type";
+import { Importance } from "../enums/importance";
+import { LighthouseService } from "../services/lighthouse.service";
+
+export class CssMinificationTest extends Test {
+    constructor () {
+        super('Minified CSS', 'Minifying CSS files can reduce network payload sizes.', 'https://web.dev/unminified-css/')
+    }
+
+    getType (): ResultType {
+        return this.isValid ? ResultType.Success : ResultType.Error
+    }
+
+    getImportance (): Importance {
+        return Importance.High
+    }
+
+    async test (): Promise<Result> {
+        const lighthouse = LighthouseService.get(this.browser.getUrl())
+        const report = lighthouse.getReport()['unminified-css']
+        this.isValid = report.score === 1
+        return this.getResult()
+    }
+}
